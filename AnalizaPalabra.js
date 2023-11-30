51cba3416e5b1628e645d897baea330ed3fec42e
Misael Castillo Rios <144559985+MisaelCast@users.noreply.github.com>
2023-11-30 00:35:35 -0800

    Update AnalizaPalabra.js

--- AnalizaPalabra.js
+++ AnalizaPalabra.js
@@ -5,47 +5,70 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function contarSilabas(palabra) {
-  palabra = palabra.toLowerCase();
-  const vocal= /[aeiouyáéíóú]/gi;
-  const vocales = palabra.match(vocal);
 
-  if (!vocales) {
-    return 0;
+
+function analizarPalabra(palabra) {
+  const encontradas = [];
+  const todasLasVocales = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú'];
+
+  for (let i = 0; i < palabra.length; i++) {
+    const letra = palabra[i];
+    if (todasLasVocales.includes(letra)) {
+      encontradas.push(letra);
+    }
   }
 
-  return vocales.length;
+  console.log(`Vocales encontradas en '${palabra}': ${encontradas}`);
+  return encontradas;
 }
 
-function esAguda(palabra) {
-  palabra = palabra.toLowerCase().trim();
-  const ultimaLetra = palabra[palabra.length - 1];
-  const vocales = 'aeiouáéíóú';
-  return (
-    vocales.includes(ultimaLetra) ||
-    ultimaLetra === 'n' ||
-    ultimaLetra === 's'
-  );
+function esEsdrujula(vocalesEncontradas) {
+  if (vocalesEncontradas.length >= 3) {
+    const antepenultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 3];
+    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
+    
+    if (acentuadas.includes(antepenultimaVocal)) {
+      console.log(`La palabra es esdrújula.`);
+    } else {
+      console.log(`La palabra no es esdrújula.`);
+    }
+  } else {
+    console.log(`La palabra no tiene suficientes vocales para ser esdrújula.`);
+  }
 }
+
+
+function esSobreesdrujula(vocalesEncontradas) {
+  if (vocalesEncontradas.length >= 4) {
+    const antesAntepenultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 4];
+    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
+    
+    if (acentuadas.includes(antesAntepenultimaVocal)) {
+      console.log(`La palabra es sobreesdrújula.`);
+    } else {
+      console.log(`La palabra no es sobreesdrújula.`);
+    }
+  } else {
+    console.log(`La palabra no tiene suficientes vocales para ser sobreesdrújula.`);
+  }
+}
+
+
 
 function procesarEntrada(palabra) {
   if (palabra === 'EXIT') {
     console.log('Saliendo del programa...');
     rl.close();
   } else {
-    const numeroSilabas = contarSilabas(palabra);
-    const esPalabraAguda = esAguda(palabra);
-    
-    console.log(`La palabra '${palabra}' tiene ${numeroSilabas} sílabas.`);
-    if (esPalabraAguda) {
-      console.log(`La palabra '${palabra}' es aguda.`);
-    } else {
-      console.log(`La palabra '${palabra}' no es aguda.`);
+    const encontradas = analizarPalabra(palabra);
+    const esdr = esEsdrujula(encontradas);
+    if (!esdr) {
+      esSobreesdrujula(encontradas);
     }
-
     pedirEntrada();
   }
 }
+
 
 function pedirEntrada() {
   rl.question('Ingresa una palabra (escribe EXIT para salir): ', (input) => {
