8b98881923f295b4c8d7da0a2d44f2e97d6d94fe
Misael Castillo Rios <144559985+MisaelCast@users.noreply.github.com>
2023-11-29 22:27:10 -0800

    Update AnalizaPalabra.js

--- AnalizaPalabra.js
+++ AnalizaPalabra.js
@@ -1,43 +1,56 @@
-function analizarPalabra(palabra) {
-  // Obtener la última letra de la palabra
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function contarSilabas(palabra) {
+  palabra = palabra.toLowerCase();
+  const vocal= /[aeiouyáéíóú]/gi;
+  const vocales = palabra.match(vocal);
+
+  if (!vocales) {
+    return 0;
+  }
+
+  return vocales.length;
+}
+
+function esAguda(palabra) {
+  palabra = palabra.toLowerCase().trim();
   const ultimaLetra = palabra[palabra.length - 1];
-
-  // Verificar si la palabra es aguda
-  if (ultimaLetra === "a" || ultimaLetra === "e" || ultimaLetra === "i" || ultimaLetra === "o" || ultimaLetra === "u" || ultimaLetra === "n" || ultimaLetra === "s") {
-    return {
-      llevaAcento: true,
-      tipo: "Aguda"
-    };
-  }
-
-  // Verificar si la palabra es llana o grave
-  if (ultimaLetra === "a" || ultimaLetra === "e" || ultimaLetra === "i" || ultimaLetra === "o" || ultimaLetra === "u") {
-    return {
-      llevaAcento: false,
-      tipo: "Llana"
-    };
-  }
-
-  // Verificar si la palabra es esdrújula
-  return {
-    llevaAcento: true,
-    tipo: "Esdrújula"
-  };
+  const vocales = 'aeiouáéíóú';
+  return (
+    vocales.includes(ultimaLetra) ||
+    ultimaLetra === 'n' ||
+    ultimaLetra === 's'
+  );
 }
 
-// Bucle principal
-while (true) {
-  // Solicitar la palabra al usuario
-  const palabra = prompt("Ingrese una palabra: ");
+function procesarEntrada(palabra) {
+  if (palabra === 'EXIT') {
+    console.log('Saliendo del programa...');
+    rl.close();
+  } else {
+    const numeroSilabas = contarSilabas(palabra);
+    const esPalabraAguda = esAguda(palabra);
+    
+    console.log(`La palabra '${palabra}' tiene ${numeroSilabas} sílabas.`);
+    if (esPalabraAguda) {
+      console.log(`La palabra '${palabra}' es aguda.`);
+    } else {
+      console.log(`La palabra '${palabra}' no es aguda.`);
+    }
 
-  // Analizar la palabra
-  const resultado = analizarPalabra(palabra);
-
-  // Imprimir el resultado
-  console.log("La palabra \"" + palabra + "\" " + (resultado.llevaAcento ? "lleva" : "no lleva") + " tilde y es de tipo " + resultado.tipo);
-
-  // Si la palabra es "EXIT", salir del programa
-  if (palabra === "EXIT") {
-    break;
+    pedirEntrada();
   }
 }
+
+function pedirEntrada() {
+  rl.question('Ingresa una palabra (escribe EXIT para salir): ', (input) => {
+    procesarEntrada(input);
+  });
+}
+
+pedirEntrada();
