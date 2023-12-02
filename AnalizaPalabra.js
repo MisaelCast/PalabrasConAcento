dbeae3c5044f78b6641d50c0e140ef8449bbb540
Misael Castillo Rios <144559985+MisaelCast@users.noreply.github.com>
2023-12-01 23:24:25 -0800

    Update AnalizaPalabra.js
    se comento el codigo y se hizo una nueva validacion con letras mayusculas

--- AnalizaPalabra.js
+++ AnalizaPalabra.js
@@ -5,11 +5,17 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Variables globales para las vocales acentuadas y letras finales
+const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
+const letrasFinales = ['n', 's', 'a', 'e', 'i', 'o', 'u'];
+
+// Función para analizar la palabra y encontrar vocales y acentos
 function analizarPalabra(palabra) {
   const encontradas = [];
   const todasLasVocales = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú'];
   let contadorAcentuadas = 0; // Contador para las vocales acentuadas
 
+  // Recorre cada letra de la palabra
   for (let i = 0; i < palabra.length; i++) {
     const letra = palabra[i];
     if (todasLasVocales.includes(letra)) {
@@ -20,20 +26,21 @@ function analizarPalabra(palabra) {
     }
   }
 
+  // Si hay más de una vocal acentuada, la palabra está mal escrita
   if (contadorAcentuadas > 1) {
     console.log(`La palabra está mal escrita, tiene más de una vocal acentuada.`);
     return false; // Indica que la palabra está mal escrita
   }
 
-  return encontradas;
+  return encontradas; // Retorna las vocales encontradas
 }
 
-
+// Función para verificar si la palabra es esdrújula
 function esEsdrujula(vocalesEncontradas) {
   if (vocalesEncontradas.length >= 3) {
     const antepenultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 3];
-    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
 
+    // Verifica si la antepenúltima vocal es acentuada
     if (acentuadas.includes(antepenultimaVocal)) {
       console.log(`La palabra es esdrújula y lleva tilde.`);
       return true; // Indica que la palabra es esdrújula
@@ -47,12 +54,13 @@ function esEsdrujula(vocalesEncontradas) {
   }
 }
 
+// Función para verificar si la palabra es sobreesdrújula
 function esSobreesdrujula(vocalesEncontradas) {
   if (vocalesEncontradas.length >= 4) {
     const antesAntepenultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 4];
     const antesAntepenultimaVocal2 = vocalesEncontradas[vocalesEncontradas.length - 5];
-    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
 
+    // Verifica si la antes antepenúltima vocal es acentuada
     if (acentuadas.includes(antesAntepenultimaVocal) || acentuadas.includes(antesAntepenultimaVocal2)) {
       console.log(`La palabra es sobreesdrújula.`);
       return true; // Indica que la palabra es sobreesdrújula
@@ -66,41 +74,36 @@ function esSobreesdrujula(vocalesEncontradas) {
   }
 }
 
-
-
+// Función para verificar si la palabra es grave
 function esGrave(vocalesEncontradas, palabra) {
   if (vocalesEncontradas.length >= 2) {
     const penultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 2];
-    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
-    const letrasFinales = ['n', 's', 'a', 'e', 'i', 'o', 'u'];
 
-
-    if (acentuadas.includes(penultimaVocal)){
-
-        if (!letrasFinales.includes(palabra[palabra.length - 1])) {
+    // Verifica si la penúltima vocal es acentuada
+    if (acentuadas.includes(penultimaVocal)) {
+      // Verifica si la última letra no es una letra final
+      if (!letrasFinales.includes(palabra[palabra.length - 1])) {
         console.log(`La palabra es grave y lleva tilde.`);
-        }else{
-         console.log(`La palabra es grave y no lleva tilde.`);
-        }
-        return true;
+      } else {
+        console.log(`La palabra es grave y no lleva tilde.`);
+      }
+      return true; // Indica que la palabra es grave
     } else {
       console.log(`La palabra no es grave.`);
-      return false;
+      return false; // Indica que la palabra no es grave
     }
   } else {
     console.log(`La palabra no puede ser grave.`);
-    return false;
+    return false; // Indica que la palabra no puede ser grave
   }
 }
 
-
-
+// Función para verificar si la palabra es aguda
 function esAguda(vocalesEncontradas, palabra) {
   if (vocalesEncontradas.length >= 1) {
     const ultimaVocal = vocalesEncontradas[vocalesEncontradas.length - 1];
-    const acentuadas = ['á', 'é', 'í', 'ó', 'ú'];
-    const letrasFinales = ['n', 's', 'a', 'e', 'i', 'o', 'u'];
 
+    // Verifica si la última vocal es acentuada o si la última letra es una letra final
     if (acentuadas.includes(ultimaVocal) || letrasFinales.includes(palabra[palabra.length - 1])) {
       console.log(`La palabra es aguda y lleva tilde.`);
     } else {
@@ -111,8 +114,7 @@ function esAguda(vocalesEncontradas, palabra) {
   }
 }
 
-
-
+// Función principal para procesar la entrada de palabras
 function procesarEntrada(palabra) {
   if (palabra === 'EXIT') {
     console.log('Saliendo del programa...');
@@ -124,40 +126,35 @@ function procesarEntrada(palabra) {
       const esSobreesdrujulaResult = esSobreesdrujula(encontradas);
 
       if (esSobreesdrujulaResult) {
-        pedirEntrada(); // Pedir una nueva palabra
-        return; // Salir de la función sin continuar con las otras validaciones
+        pedirEntrada(); // Pide una nueva palabra
+        return; // Sale de la función sin continuar con las otras validaciones
       } else {
         const esEsdrujulaResult = esEsdrujula(encontradas);
 
         if (esEsdrujulaResult) {
-          pedirEntrada(); // Pedir una nueva palabra
-          return; // Salir de la función sin continuar con las otras validaciones
+          pedirEntrada(); // Pide una nueva palabra
+          return; // Sale de la función sin continuar con las otras validaciones
         } else {
           const esGraveResult = esGrave(encontradas, palabra);
 
           if (esGraveResult) {
-            pedirEntrada(); // Pedir una nueva palabra
-            return; // Salir de la función sin continuar con las otras validaciones
+            pedirEntrada(); // Pide una nueva palabra
+            return; // Sale de la función sin continuar con las otras validaciones
           } else {
-            esAguda(encontradas, palabra);
+            esAguda(encontradas, palabra); // Verifica si es aguda
           }
         }
       }
     }
-    pedirEntrada(); // Pedir una nueva palabra independientemente del resultado
+    pedirEntrada(); // Pide una nueva palabra independientemente del resultado
   }
 }
 
-////////////////////////////PALABRAS PRUEBAS  GRAVE fácil,  café AGUDA  ESDRUJULA teléfono, SOBRE académicamente.
-
-
-
+// Función para solicitar la entrada de palabras al usuario
 function pedirEntrada() {
   rl.question('Ingresa una palabra (escribe EXIT para salir): ', (input) => {
-    procesarEntrada(input);
+    procesarEntrada(input.toLowerCase()); // Procesa la entrada en minúsculas
   });
 }
 
-pedirEntrada();
-
-
+pedirEntrada(); // Inicia el programa solicitando la primera palabra
